94e32306a150489ad40aa86afab0becc40e2af01
abu-umair <mrdwnw93@gmail.com>
2025-11-17 15:59:29 +0700

    ~ edit komentar

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { Counter } from "@/app/counter";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1> //?server componen (karena tidak ada layout / static)
-      <Counter /> //?client componen (karena ada layout/abstract / dinamis)
+      <h1>Home</h1> {/*? //?server componen (karena tidak ada layout / static) */}
+      <Counter /> {/* //?client componen (karena ada layout/abstract / dinamis) */}
     </div>
   );
 }
