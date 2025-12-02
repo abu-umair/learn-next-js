ed2638b8f5becd3a29ece6144ae0d1919712b6e9
abu-umair <mrdwnw93@gmail.com>
2025-12-02 14:59:39 +0700

    + menampilkan gambar eksternal

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { Counter } from "@/app/counter";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1> {/*? //?server componen (karena tidak ada layout / static) */}
       <Counter /> {/* //?client componen (karena ada layout/abstract / dinamis) */}
+      <Image
+        width={200}
+        height={200}
+        alt="Gambar Kucing"
+        src="https://plus.unsplash.com/premium_photo-1677101221533-52b45823a2dc?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+
     </div>
   );
 }
