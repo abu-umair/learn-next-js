be6086e117b3a8b47c1627429aececfa721b5416
abu-umair <mrdwnw93@gmail.com>
2025-12-03 16:03:59 +0700

    ~ mengembalikan semula

--- app/header.tsx
+++ app/header.tsx
@@ -1,18 +1,15 @@
 import { getSetting } from "@/app/queries/getSetting";
 import { Nav } from "./nav";
-import { Suspense } from "react";
 
 
 export async function Header() {
-    const setting = getSetting(); //?mengambil data setting (1x)
+    const setting = await getSetting(); //?mengambil data setting (1x)
 
     console.log(setting);
     return (
         <header className="border-b border-white py-2 mb-2">
-            <Suspense fallback={<div>Loading...</div>} > {/*?menggunakan suspense untuk menampilkan loading ketika data belum siap/masih ngebaca promise/STREAMING */}
-                {/* <div className="text-2xl">{setting.siteName}</div> */}
-                <Nav settingPromise={setting} />
-            </Suspense>
+            <div className="text-2xl">{setting.siteName}</div>
+            <Nav />
         </header>
     )
 }
--- app/nav.tsx
+++ app/nav.tsx
@@ -1,21 +1,9 @@
-"use client";
 import Link from 'next/link'
-import { Setting } from '@/app/types/Setting';
-import { use } from 'react';
 
-export function Nav({
-    settingPromise
-}: {
-    settingPromise: Promise<Setting>
-}) {
-    
-    const setting = use(settingPromise);
-    console.log(setting);
-    
-
+export function Nav() {
     return (
         <nav className="space-x-4">
-            <Link href="/">Home {setting.siteName}</Link>
+            <Link href="/">Home</Link>
             <Link href="/about">About</Link>
             <Link href="/about/team">Team</Link>
         </nav>
