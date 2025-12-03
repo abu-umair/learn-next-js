6e2318385a05520c0c4656f09b0c79fbbdae5c56
abu-umair <mrdwnw93@gmail.com>
2025-12-03 09:14:27 +0700

    + terdapat error jika seperti ini di client component

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,10 +1,13 @@
 "use client";
 import Link from 'next/link'
+import { getSetting } from '@/app/queries/getSetting';
 
-export default function Nav() {
+export default async function Nav() {
+    const setting = await getSetting();
+
     return (
         <nav className="space-x-4">
-            <Link href="/">Home</Link>
+            <Link href="/">Home {setting.siteName}</Link>
             <Link href="/about">About</Link>
             <Link href="/about/team">Team</Link>
         </nav>
