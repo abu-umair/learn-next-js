057a19e209f744ed8dd45cd6c32647dc34cfa13b
abu-umair <mrdwnw93@gmail.com>
2025-12-04 08:49:12 +0700

    + membuat validasi

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,13 @@
 'use server'; //?memberi tanda jika ini action
 
 
-export async function createComment(formData: FormData) {
+export async function createComment(prevData: any, formData: FormData) {
     console.log(formData.get("comment")); //?comment: nama inputan di form
+    const comment = formData.get("comment") as string;
+
+    if(!comment){
+        return {
+            error: "Comment is required"
+        }
+    }
 }
--- app/posts/[slug]/comment-form.tsx
+++ app/posts/[slug]/comment-form.tsx
@@ -1,8 +1,21 @@
+"use client";
 import { createComment } from "@/app/actions"
+import { useActionState } from "react"
+
+const initialState = {
+    error: "", //? initial state dari error yang ada di action (line 10)
+}
 
 export function CommentForm() {
+    const [state, formAction, pending] = useActionState(createComment, initialState)
+
     return (
-        <form className="flex flex-col w-72" action={createComment}>
+        <form className="flex flex-col w-72" action={formAction}>
+            {state ?.error && (
+                <div className="text-red-500" role="alert">
+                    {state.error}
+                </div>
+            )}
             <textarea
                 name="comment"
                 placeholder="Write your comment here..."
